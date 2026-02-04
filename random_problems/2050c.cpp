10345c64a0cbdfdcbe3fd341fca76a54dcbf8911
Mridul Sharma <mridulsharma2034@gmail.com>
2026-02-04 17:28:19 +0530

    4feb

--- random_problems/2050c.cpp
+++ random_problems/2050c.cpp
@@ -1,4 +1,47 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int t;
+//     cin >> t;
+
+//     while (t--) {
+//         string n;
+//         cin >> n;
+
+//         int sum = 0;
+//         int two = 0;
+//         int three = 0;
+
+//         for (int i = 0; i < n.size(); i++) {
+//             int d = n[i] - '0';
+//             sum += d;
+//             if (d == 2) two++;
+//             if (d == 3) three++;
+//         }
+
+//         bool flag = false;
+//         int rem = sum % 9;
+
+        
+//         for (int a = 0; a <= min(8, two); a++) {
+//             for (int b = 0; b <= min(2, three); b++) {
+//                 if ((rem + 2 * a + 6 * b) % 9 == 0) {
+//                     flag = true;
+//                 }
+//             }
+//         }
+
+//         if (flag) cout << "YES\n";
+//         else cout << "NO\n";
+//     }
+
+//     return 0;
+// }
+fix this code --#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -10,33 +53,35 @@ int main() {
 
     while (t--) {
         string n;
-        cin >> n;
-
-        int sum = 0;
-        int two = 0;
-        int three = 0;
-
-        for (int i = 0; i < n.size(); i++) {
-            int d = n[i] - '0';
-            sum += d;
-            if (d == 2) two++;
-            if (d == 3) three++;
+        cin >> n;   
+        int sum=0;
+        int two=0;
+        int three=0;
+        for(int i=0;i<n.size();i++){
+            sum+=(n[i]-'0');
+            if((n[i]-'0')==3 ) three++;
+            if((n[i]-'0')==2 ) two++;
         }
-
-        bool flag = false;
-        int rem = sum % 9;
-
-        
-        for (int a = 0; a <= min(8, two); a++) {
-            for (int b = 0; b <= min(2, three); b++) {
-                if ((rem + 2 * a + 6 * b) % 9 == 0) {
-                    flag = true;
-                }
+        bool flag=false;
+        int rem = sum%9;
+        if(rem%2==0 && sum>9){
+            if(rem/2<=two ){
+                flag=true;
             }
+            else if(rem%6==0){
+                flag==true;
+                
+            }
+            
         }
+        else {
+            if(9-sum==6 || three>0) flag==true;
+            else if((9-sum)%2==0 && two >=(9-sum)%2);
 
-        if (flag) cout << "YES\n";
-        else cout << "NO\n";
+        }
+        if(flag)cout<<"YES"<<endl;
+        else cout << "NO"<<endl;
+
     }
 
     return 0;
--- random_problems/tempCodeRunnerFile.cpp
+++ random_problems/tempCodeRunnerFile.cpp
@@ -1,41 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-
     while (t--) {
-        int n;
-        cin>>n;
-        vector<int>a(n);
-        bool z= false;
-        for(int i =0;i<n;i++){
-            cin>>a[i];
-            if(a[i]==0){
-                z=true;
-            }
+        ll w, b;
+        cin >> w >> b;
 
-        }
-        if(!z){
-                cout << 1 << endl;
-                cout << 1 <<" "<< n<< endl;
-                
-            }
-            else if (a[n]==0){
-                cout << 3<< endl;
-                cout << n-1 <<" "<< n<< endl;
-                cout << 1 <<" "<< n-2<< endl;
-                cout<< 1 << " " << 2 << endl;
-            }
-            else {
-                for(int i=0;i<n;i++){
-                    cout<<1<<" "<<2<< endl;
-                }
-            }
+        ll total = w + b;
+        ll k = (ll)((sqrt(1 + 8.0 * total) - 1) / 2);
 
+        cout << k << "\n";
     }
     return 0;
 }
